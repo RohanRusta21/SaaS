3797b7fca861477a35ddefae78485c7563e95ca6
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2023-08-20 13:31:33 +0000

    "submit button"

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -67,7 +67,7 @@ const Contact = () => {
                   </div>
                   <div className="w-full px-4">
                     <button className="rounded-md bg-primary py-4 px-9 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
-                      Submit Ticket
+                      Submit
                     </button>
                   </div>
                 </div>
